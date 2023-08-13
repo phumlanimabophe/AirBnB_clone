b670b7effb5fee9ed8534a4cd2d2dd1cf0321cd0
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-08-13 19:06:47 +0000

    try again

--- models/state.py
+++ models/state.py
@@ -4,13 +4,15 @@
 It defines one class, `State(),
 which sub-classes the `BaseModel()` class.`
 """
+# Import the necessary base model class
 from models.base_model import BaseModel
 
-
+# Define a class that represents a State and inherits from BaseModel
 class State(BaseModel):
     """A state in the application.
 
     Attributes:
-        name
+        name    # Name of the state
     """
+    # Initialize the state name as an empty string
     name = ""
--- models/user.py
+++ models/user.py
@@ -1,10 +1,14 @@
 #!usr/bin/python3
+# Import the necessary base model class
 from models.base_model import BaseModel
-
 
 class User(BaseModel):
     """Creates a new user"""
+    # Initialize user email as an empty string
     email = ""
+    # Initialize user password as an empty string
     password = ""
+    # Initialize user's first name as an empty string
     first_name = ""
+    # Initialize user's last name as an empty string
     last_name = ""
