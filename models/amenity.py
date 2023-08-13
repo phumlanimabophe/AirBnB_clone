15be89d1a9f0c16d0b5867e1d7762078669b6b92
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-08-13 19:14:57 +0000

    try again

--- models/amenity.py
+++ models/amenity.py
@@ -4,14 +4,15 @@
 It defines one class, `Amenity(),
 which sub-classes the `BaseModel()` class.`
 """
+# Import the necessary base model class
 from models.base_model import BaseModel
 
-
+# Define a class that represents an Amenity and inherits from BaseModel
 class Amenity(BaseModel):
     """An amenity provided by a place/house.
 
     Attributes:
-        name
+        name    # Name of the amenity
     """
-
-    name = ""
+    # Initialize the amenity name as an empty string
+    name = ""   
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,25 +4,36 @@ Module: file_storage.py
 
 Defines a `FileStorage` class.
 """
+
+# Import necessary modules and classes
+# Import the 'os' module
 import os
+# Import the 'json' module         
 import json
+# Import the 'BaseModel' class
 from models.base_model import BaseModel
+# Import the 'User' class
 from models.user import User
+# Import the 'State' class              
 from models.state import State
+# Import the 'City' class             
 from models.city import City
+# Import the 'Review' class               
 from models.review import Review
+# Import the 'Amenity' class           
 from models.amenity import Amenity
-from models.place import Place
+# Import the 'Place' class         
+from models.place import Place             
 
-
+# Define a class for serializing instances to a JSON file and deserializing JSON file to instances
 class FileStorage():
     """
     serializes instances to a JSON file and
     deserializes JSON file to instances
     """
 
-    __file_path = "file.json"
-    __objects = {}
+    __file_path = "file.json"    # Path to the JSON file
+    __objects = {}               # Dictionary to store objects
 
     def all(self):
         """
@@ -41,7 +52,9 @@ class FileStorage():
         """
         serializes __objects to the JSON file (path: __file_path)
         """
+        # Open the JSON file in write mode
         with open(FileStorage.__file_path, 'w') as f:
+            # Serialize objects to JSON and write to the file
             json.dump(
                 {k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
 
@@ -50,24 +63,30 @@ class FileStorage():
         deserializes the JSON file to __objects only if the JSON
         file exists; otherwise, does nothing
         """
+        # Dictionary mapping class names to their respective classes
         current_classes = {'BaseModel': BaseModel, 'User': User,
                            'Amenity': Amenity, 'City': City, 'State': State,
                            'Place': Place, 'Review': Review}
 
+        # Check if the JSON file exists
         if not os.path.exists(FileStorage.__file_path):
             return
 
+        # Open the JSON file in read mode
         with open(FileStorage.__file_path, 'r') as f:
             deserialized = None
 
             try:
+                # Attempt to load the JSON data from the file
                 deserialized = json.load(f)
             except json.JSONDecodeError:
                 pass
 
+            # If deserialization was successful, update __objects
             if deserialized is None:
                 return
 
+            # Populate __objects with deserialized instances
             FileStorage.__objects = {
                 k: current_classes[k.split('.')[0]](**v)
                 for k, v in deserialized.items()}
