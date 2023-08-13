d07812e971d6786cf7c3af0402a05d25cfdcff87
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-08-13 19:48:21 +0000

    try again

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
 """Unit tests for the `city` module.
 """
-import os
-import unittest
-from models.engine.file_storage import FileStorage
+# Import the 'os' module
+import os     
+# Import the 'unittest' module             
+import unittest    
+# Import the 'FileStorage' class from the 'models.engine.file_storage' module        
+from models.engine.file_storage import FileStorage   
+# Import the 'Place' class from the 'models.place' module
 from models.place import Place
-from models import storage
-from datetime import datetime
+# Import the 'storage' module from the 'models' package   
+from models import storage   
+# Import the 'datetime' class from the 'datetime' module
+from datetime import datetime   
 
-
+# Define a class for testing the 'Place' class
 class TestPlace(unittest.TestCase):
     """Test cases for the `Place` class."""
 
+    # Set up any initial conditions for the tests
     def setUp(self):
         pass
 
+    # Perform cleanup after each test
     def tearDown(self) -> None:
         """Resets FileStorage data."""
         FileStorage._FileStorage__objects = {}
         if os.path.exists(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
+    # Test method for class attributes
     def test_params(self):
         """Test method for class attributes"""
-
+        # Create instances of Place class with different parameters
         p1 = Place()
         p3 = Place("hello", "wait", "in")
+        # Construct a key based on the instance's class name and ID
         k = f"{type(p1).__name__}.{p1.id}"
+        # Validate instance attribute types and presence in storage
         self.assertIsInstance(p1.name, str)
         self.assertIn(k, storage.all())
         self.assertEqual(p3.name, "")
@@ -43,40 +54,55 @@ class TestPlace(unittest.TestCase):
         self.assertIsInstance(p1.latitude, float)
         self.assertIsInstance(p1.amenity_ids, list)
 
+    # Test method for public instance attributes
     def test_init(self):
         """Test method for public instances"""
-
+        # Create instances of Place class
         p1 = Place()
         p2 = Place(**p1.to_dict())
+        # Validate instance attribute types and consistency
         self.assertIsInstance(p1.id, str)
         self.assertIsInstance(p1.created_at, datetime)
         self.assertIsInstance(p1.updated_at, datetime)
         self.assertEqual(p1.updated_at, p2.updated_at)
 
+    # Test method for str representation
     def test_str(self):
         """Test method for str representation"""
+        # Create an instance of Place class
         p1 = Place()
+        # Construct the expected string representation
         string = f"[{type(p1).__name__}] ({p1.id}) {p1.__dict__}"
+        # Validate the instance's string representation
         self.assertEqual(p1.__str__(), string)
 
+    # Test method for 'save'
     def test_save(self):
         """Test method for save"""
+        # Create an instance of Place class
         p1 = Place()
+        # Record the current 'updated_at' attribute value
         old_update = p1.updated_at
+        # Call the 'save' method
         p1.save()
+        # Validate that 'updated_at' attribute changed
         self.assertNotEqual(p1.updated_at, old_update)
 
+    # Test method for 'to_dict'
     def test_todict(self):
         """Test method for dict"""
+        # Create instances of Place class
         p1 = Place()
         p2 = Place(**p1.to_dict())
+        # Obtain the dictionary representation using 'to_dict'
         a_dict = p2.to_dict()
+        # Validate the dictionary representation
         self.assertIsInstance(a_dict, dict)
         self.assertEqual(a_dict['__class__'], type(p2).__name__)
         self.assertIn('created_at', a_dict.keys())
         self.assertIn('updated_at', a_dict.keys())
         self.assertNotEqual(p1, p2)
 
-
+# Execute the tests if this script is the main program
 if __name__ == "__main__":
     unittest.main()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,70 +1,97 @@
 #!/usr/bin/python3
 """Unit tests for the `review` module.
 """
-import os
+# Import the 'os' module
+import os   
+# Import the 'unittest' module               
 import unittest
-from models.review import Review
+# Import the 'Review' class from the 'models.review' module            
+from models.review import Review 
+# Import the 'storage' module from the 'models' package  
 from models import storage
+# Import the 'datetime' class from the 'datetime' module   
 from datetime import datetime
-from models.engine.file_storage import FileStorage
+# Import the 'FileStorage' class from the 'models.engine.file_storage' module
+from models.engine.file_storage import FileStorage   
 
-
+# Define a class for testing the 'Review' class
 class TestReview(unittest.TestCase):
     """Test cases for the `Review` class."""
 
+    # Set up any initial conditions for the tests
     def setUp(self):
         pass
 
+    # Perform cleanup after each test
     def tearDown(self) -> None:
         """Resets FileStorage data."""
         FileStorage._FileStorage__objects = {}
         if os.path.exists(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
+    # Test method for class attributes
     def test_params(self):
         """Test method for class attributes"""
-
+        # Create instances of Review class with different parameters
         r1 = Review()
         r3 = Review("hello", "wait", "in")
+        # Construct a key based on the instance's class name and ID
         k = f"{type(r1).__name__}.{r1.id}"
+        # Validate instance attribute types and values
         self.assertIsInstance(r1.text, str)
         self.assertIsInstance(r1.user_id, str)
         self.assertIsInstance(r1.place_id, str)
         self.assertEqual(r3.text, "")
 
+    # Test method for public instance attributes
     def test_init(self):
         """Test method for public instances"""
+        # Create instances of Review class
         r1 = Review()
         r2 = Review(**r1.to_dict())
+        # Validate instance attribute types and consistency
         self.assertIsInstance(r1.id, str)
         self.assertIsInstance(r1.created_at, datetime)
         self.assertIsInstance(r1.updated_at, datetime)
         self.assertEqual(r1.updated_at, r2.updated_at)
 
+    # Test method for str representation
     def test_str(self):
         """Test method for str representation"""
+        # Create an instance of Review class
         r1 = Review()
+        # Construct the expected string representation
         string = f"[{type(r1).__name__}] ({r1.id}) {r1.__dict__}"
+        # Validate the instance's string representation
         self.assertEqual(r1.__str__(), string)
 
+    # Test method for 'save'
     def test_save(self):
         """Test method for save"""
+        # Create an instance of Review class
         r1 = Review()
+        # Record the current 'updated_at' attribute value
         old_update = r1.updated_at
+        # Call the 'save' method
         r1.save()
+        # Validate that 'updated_at' attribute changed
         self.assertNotEqual(r1.updated_at, old_update)
 
+    # Test method for 'to_dict'
     def test_todict(self):
         """Test method for dict"""
+        # Create instances of Review class
         r1 = Review()
         r2 = Review(**r1.to_dict())
+        # Obtain the dictionary representation using 'to_dict'
         a_dict = r2.to_dict()
+        # Validate the dictionary representation
         self.assertIsInstance(a_dict, dict)
         self.assertEqual(a_dict['__class__'], type(r2).__name__)
         self.assertIn('created_at', a_dict.keys())
         self.assertIn('updated_at', a_dict.keys())
         self.assertNotEqual(r1, r2)
 
-
+# Execute the tests if this script is the main program
 if __name__ == "__main__":
     unittest.main()
